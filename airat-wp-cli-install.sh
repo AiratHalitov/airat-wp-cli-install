dcf4665a070208db5fcbf3d1766682d8665d180c
Airat Halitov <airat.halitov@gmail.com>
2020-12-18 17:11:06 +0500

    sudo

--- airat-wp-cli-install.sh
+++ airat-wp-cli-install.sh
@@ -9,10 +9,10 @@ rm -rf wp-completion.bash
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null 2>&1
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+sudo mv wp-cli.phar /usr/local/bin/wp
 
 wget https://raw.githubusercontent.com/wp-cli/wp-cli/v2.4.0/utils/wp-completion.bash > /dev/null 2>&1
-mv wp-completion.bash /usr/local/bin/wp-completion.bash
+sudo mv wp-completion.bash /usr/local/bin/wp-completion.bash
 
 if [ ! -f bashrc-updated.txt ]; then
     echo "source /usr/local/bin/wp-completion.bash" >> ~/.bashrc
